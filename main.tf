955e6065e2df5e379dae89b33e6a0e225921cd10
Pankhuri22-P <pankhurip30@gmail.com>
2026-01-16 16:56:48 +0100

    Intial commit

--- main.tf
+++ main.tf
@@ -1,1 +1,51 @@
-# YOU MAIN CODE HERE
+resource "aws_cloudfront_distribution" "example_distribution" {
+    aliases = var.alternate_domain_names
+    default_cache_behavior {
+      allowed_methods = var.allowed_methods    
+      cached_methods = var.cached_methods
+      viewer_protocol_policy = var.viewer_protocol_policy
+      target_origin_id = var.origin_id
+      forwarded_values {
+        query_string = false
+        cookies {
+         forward = "none"
+        }
+     }
+    } 
+    dynamic ordered_cache_behavior {
+      for_each = var.ordered_cache_behaviour
+      content{
+        allowed_methods = each.value.ordered_cache_allowed_methods
+        cached_methods = each.value.ordered_cached_methods
+        viewer_protocol_policy = each.value.ordered_viewer_protocol_policy
+        path_pattern = each.value.ordered_cached_origin_path_pattern
+        target_origin_id = each.value.ordered_cached_origin_id
+      }
+    } 
+
+    dynamic custom_error_response {
+      for_each = var.custom_error_response
+      content{
+        error_caching_min_ttl = each.value.error_caching_min_ttl
+        error_code = each.value.error_code
+        response_code = each.value.response_code
+
+      }
+      
+    }
+    enabled = true
+    origin {
+        domain_name = var.domain_name
+        origin_id = var.origin_id
+        origin_path = var.origin_path
+    }
+    restrictions {
+      geo_restriction {
+        locations = [  ]
+        restriction_type = "none"
+      }
+    }
+    viewer_certificate {
+      cloudfront_default_certificate = true
+    }
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,53 @@
-# VARIABLES
+variable "allowed_methods"{
+  description = "allowed http methods"
+  type = list(string)
+}
+
+variable "cached_methods"{
+  type = list(string)
+}
+
+variable "viewer_protocol_policy" {
+  type = string
+}
+
+variable "domain_name" {
+  type = string
+}
+
+variable "origin_id" {
+  type = string
+}
+
+variable "origin_path" {
+  type = string
+  default = ""
+}
+
+variable "alternate_domain_names" {
+  type = list(string)
+  default = []
+}
+
+variable "ordered_cache_behaviour"{
+  type = list(object({
+    ordered_cache_allowed_methods = list(string)
+    ordered_cached_methods = list(string)
+    ordered_cached_viewer_protocol_policy = string
+    ordered_cached_origin_id = string
+    ordered_cached_origin_path_pattern = string
+    ordered_cached_domain_name = string
+  }))
+  default = []
+}
+
+variable "custom_error_response"{
+  type = list(object({
+    error_caching_min_ttl = string
+    error_code = string
+    response_code = string
+    response_page_path = string
+  }))
+  default = [ {} ]
+}
+
